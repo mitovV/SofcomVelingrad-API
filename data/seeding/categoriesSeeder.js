7f750a352afdb57971c64ec8fc2bdde126bab1b8
CodingMania <vasilmitovv@gmail.com>
2024-05-26 17:23:17 +0300

    Extend categories seeder

--- data/seeding/categoriesSeeder.js
+++ data/seeding/categoriesSeeder.js
@@ -1,6 +1,6 @@
 import Category from "../models/Category.js";
 
-const categories = [
+const mainCategories = [
 
     { name: 'Злато' },
     { name: 'Сребро' },
@@ -14,12 +14,52 @@ const categories = [
 
 ]
 
+const goldAndSilverCategories = [
+    { name: 'Пръстени' },
+    { name: 'Обеци' },
+    { name: 'Висулки' },
+    { name: 'Колиета' },
+    { name: 'Гривни' },
+    { name: 'Монети' },
+    { name: 'Синджири' },
+    { name: 'Халки' },
+    { name: 'Други' },
+]
+
+const ringCategories = [
+    { name: 'Дамски' },
+    { name: 'Мъжки' },
+    { name: 'Детски' },
+]
+
 let data = await Category.find({})
 
-if (data.length  === 0) {
-    categories.forEach(categoryData => {
-        let category = new Category({name: categoryData.name})
+if (data.length === 0) {
+    let allCategories = Array.prototype.concat(mainCategories, goldAndSilverCategories, ringCategories)
 
-        category.save()
+    allCategories.forEach(async (categoryData) => {
+        let category = new Category({ name: categoryData.name })
+
+        await category.save()
     })
 }
+
+let goldCategory = await Category.findOne({ name: 'Злато' })
+let silverCategory = await Category.findOne({ name: 'Сребро' })
+
+if (goldCategory.subcategories.length === 0 && silverCategory.subcategories.length === 0) {
+
+    for (let index = 0; index < goldAndSilverCategories.length; index++) {
+        let name = goldAndSilverCategories[index].name
+        let category = await Category.findOne({ name }).lean()
+        
+        goldCategory.subcategories.push(category)
+        silverCategory.subcategories.push(category)
+    }
+
+    console.log(goldCategory.subcategories)
+    await goldCategory.save()
+    await silverCategory.save()
+}
+
+let ringCategory = Category.findOne({ name: 'Пръстени' })
