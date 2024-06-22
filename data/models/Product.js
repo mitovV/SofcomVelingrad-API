1e00a6254c88e86bc17ad5f5079c4c5d7b34ab6a
CodingMania <vasilmitovv@gmail.com>
2024-06-22 15:15:35 +0300

    Add gold carat model

--- data/models/Product.js
+++ data/models/Product.js
@@ -34,16 +34,17 @@ const productSchema = new Schema({
     },
     size: {
         type: mongoose.Types.ObjectId,
-        ref: "RingSize",
+        ref: 'RingSize',
         require: function () { return [RING_CATEGORY_NAME, WEDDING_RING_CATEGORY_NAME].includes(this.categoryName) }
     },
     goldCarat: {
-        type: String,
+        type: mongoose.Types.ObjectId,
+        ref: 'GoldCarat',
+        require: function () { return this.material === GOLD_CATEGORY_NAME },
         enum: {
             values: ['(8K)-33.33% злато', '(9K)-37.5% злато', '(10K)-41.67% злато', '(11K)-45.83% злато', '(12K)-50% злато', '13K)-54.17% злато', '(14K)-58.50% злато', '15 карата (15K) - 62.5% злато', '(16K)-66.67% злато', '(17K)-70.83% злато', '(18K)-75% злато', '(19K)-79.17% злато', '(20K)-83.33% злато', '(21K)-87.5% злато', '(22K)-91.67% злато', '(23K)-95.83% злато', '(24K)-99.9% злато'],
             message: 'Invalid carat for gold'
-        },
-        require: function () { return this.material === GOLD_CATEGORY_NAME }
+        }
     },
     silverCarat: {
         type: String,
